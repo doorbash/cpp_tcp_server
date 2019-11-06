371653e07923ff98915ef111c83093a2acff549b
Milad Doorbash <milad.doorbash@gmail.com>
2019-11-07 02:48:25 +0330

    2nd commit!

--- main.cpp
+++ main.cpp
@@ -9,7 +9,9 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
+
     int listening = socket(AF_INET, SOCK_STREAM, 0);
+
     if (listening == -1) {
         cerr << "Can't create a socket!" << endl;
         return -1;
@@ -18,8 +20,8 @@ int main(int argc, char *argv[]) {
     sockaddr_in hint;
     hint.sin_family = AF_INET;
     hint.sin_port = htons(3000);
-    inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr);
 
+    inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr);
     if (bind(listening, (sockaddr *)&hint, sizeof(hint)) == -1) {
         cerr << "Can't bind to ip/port" << endl;
         return -2;
@@ -31,7 +33,8 @@ int main(int argc, char *argv[]) {
     }
 
     sockaddr_in client;
-    socklen_t clientSize;
+    socklen_t clientSize = sizeof(client);
+
     char host[NI_MAXHOST];
     char svc[NI_MAXSERV];
 
@@ -58,8 +61,8 @@ int main(int argc, char *argv[]) {
         svc,
         NI_MAXSERV,
         0);
-    
-    if(result) {
+
+    if (result == 0) {
         cout << host << " connected on " << svc << endl;
     } else {
         inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
@@ -67,25 +70,23 @@ int main(int argc, char *argv[]) {
     }
 
     char buf[4096];
-    while(true) {
+    while (true) {
         memset(buf, 0, 4096);
         int bytesRecv = recv(clientSocket, buf, 4096, 0);
-        if(bytesRecv == -1) {
+        if (bytesRecv == -1) {
             cerr << "There was a connection issue" << endl;
             break;
         }
-
-        if(bytesRecv == 0) {
+        if (bytesRecv == 0) {
             cout << "The client disconnected" << endl;
             break;
         }
-
         cout << "Received: " << string(buf, 0, bytesRecv) << endl;
 
         send(clientSocket, buf, bytesRecv + 1, 0);
     }
 
     close(clientSocket);
-
+    
     return 0;
 }
